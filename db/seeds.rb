85a780a727b97b0be0f26c389b48ae37743b225e
Virna Harri <harrivirna18@gmail.com>
2021-10-05 21:19:08 +0200

    Created & completed  the Restaurant & Review models as well as the controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+mami_camilla =  { name: "Mami Camilla", address: "Prenzlauer Berg", category: "italian" }
+kumpel_keule =  { name: "Kumpel Keule", address: "Markthalle 9", category: "belgian" }
+maria_bonita =  { name: "Maria Bonita", address: "Prenzlauer Berg", category: "italian" }
+
+[dishoom, pizza_east, mami_camilla, kumpel_keule, maria_bonita].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
